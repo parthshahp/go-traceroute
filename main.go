48eb6f20eda84e7b35069d99b0ac8cef20ff2e5a
Parth Shah <parthshahp1@gmail.com>
2024-04-06 22:17:34 -0400

    add some stuff

--- main.go
+++ main.go
@@ -28,10 +28,11 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Could not get IPs: %v\n", err)
 		return
 	}
-	ip := ips[0]
+
+	ip := ips[0].To4()
 
 	fmt.Printf(
-		"traceroute to %s (%s), %d hops max, %d byte packets",
+		"traceroute to %s (%s), %d hops max, %d byte packets\n",
 		hostname,
 		ip,
 		maxHops,
@@ -50,10 +51,53 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Could not create socket: %v\n", err)
 		return
 	}
+
+	// Set timeout for recvSock
 	timeout := unix.Timeval{Sec: int64(timeout), Usec: 0}
 	err = unix.SetsockoptTimeval(recvSock, unix.SOL_SOCKET, unix.SO_RCVTIMEO, &timeout)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not set timeout: %v\n", err)
 		return
+	}
+
+	defer unix.Close(sendSock)
+	defer unix.Close(recvSock)
+	begin := time.Now()
+	destAddr := &unix.SockaddrInet4{Port: defaultPort, Addr: [4]byte{ip[0], ip[1], ip[2], ip[3]}}
+
+	for {
+		begin = time.Now()
+		// Set ttl
+		err = unix.SetsockoptInt(sendSock, unix.IPPROTO_IP, unix.IP_TTL, maxHops)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not set TTL: %v\n", err)
+			return
+		}
+
+		err = unix.Sendto(
+			sendSock,
+			[]byte{0},
+			0,
+			destAddr,
+		)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not send packet: %v\n", err)
+			return
+		}
+
+		var buf [packetSize]byte
+		_, _, err = unix.Recvfrom(recvSock, buf[:], 0)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not receive packet: %v\n", err)
+			return
+		}
+
+		// Get the time taken to receive the packet
+		end := time.Now()
+		duration := end.Sub(begin)
+
+		// Print the message
+		fmt.Printf(string(buf[:]), duration)
+		break
 	}
 }
